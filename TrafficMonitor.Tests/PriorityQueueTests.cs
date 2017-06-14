0a2f32dafe0ba867cbb28b39c41e1508f43e8e5c
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-06-14 11:08:14 +0200

    Adding PriorityQueue

---
+++ TrafficMonitor.Tests/PriorityQueueTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TrafficMonitor.Tests
+{
+  [TestFixture]
+  class PriorityQueueTests
+  {
+    [Test]
+    public void QueueUponCreation_ShouldBeEmpty()
+    {
+      //Arrange && Act && Assert
+      Assert.IsTrue(new PriorityQueue<TrafficEvent>().Queue.Count == 0);
+    }
+  }
+
+  internal class PriorityQueue <T> where T : IComparable<T>
+  {
+    public IList<T> Queue { get; set; }
+  }
+}
